809b44892151688cf9c5187633c6dd70f5b6baad
SrinikaSharma <93469490+SrinikaSharma@users.noreply.github.com>
2023-11-09 17:49:25 +0530

    Changes in UI

--- student-login.js
+++ student-login.js
@@ -1,36 +1,11 @@
-// Initialize Firebase with your Firebase project configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCmpwwd-pActSCa32M2WT2WGTqkndSP6_E",
-  authDomain: "dept-internship-maintenance.firebaseapp.com",
-  projectId: "dept-internship-maintenance",
-  storageBucket: "dept-internship-maintenance.appspot.com",
-  messagingSenderId: "133911448147",
-  appId: "1:133911448147:web:2a1708e751ce16c34c14a6",
-  measurementId: "G-9YRFBK03HZ"
-};
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-firebase.initializeApp(firebaseConfig);
-
-// Function to handle student login
-const studentLoginForm = document.getElementById('student-login-form');
-
-studentLoginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  // Sign in the user with Firebase Authentication
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // User is signed in, redirect to the student dashboard
-      window.location.href = 'student-dashboard.html';
-    })
-    .catch((error) => {
-      // Handle login errors
-      const errorMessage = error.message;
-      alert(`Login failed: ${errorMessage}`);
-    });
+sign_up_btn.addEventListener('click', () =>{
+    container.classList.add("sign-up-mode");
 });
 
-// Import Firebase Authentication and initialize Firebase (make sure you've already set up Firebase in your projec
+sign_in_btn.addEventListener('click', () =>{
+    container.classList.remove("sign-up-mode");
+});
